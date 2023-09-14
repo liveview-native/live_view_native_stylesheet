d5a9ab3d01ab78e19db1d612e288cd7ce6bad41c
Brian Cardarella <bcardarella@gmail.com>
2023-09-14 07:06:29 -0400

    Initial commit

---
+++ lib/live_view_native_stylesheet.ex
@@ -0,0 +1,26 @@
+defmodule LiveViewNative.Stylesheet do
+
+  def compile(platform, rules) do
+    with {:ok, platforms} <- Application.fetch_env(:live_view_native_stylesheet, :platforms),
+    {:ok, compiler} <- Keyword.fetch(platforms, platform) do
+      compiler.compile(rules)
+
+    else
+      :error -> "Make sure the compiler for platform `#{inspect(platform)}` is defined in your config for `:live_view_native_stylesheet`"
+    end
+  end
+
+  defmacro __using__(platform) do
+    quote do
+      def compile(class_list, target: target) do
+        Enum.reduce(class_list, %{}, fn(class_name, class_map) ->
+          case class(class_name, target: target) do
+            {:unmatched, _msg} -> class_map
+            rules -> Map.put(class_map, class_name, LiveViewNative.Stylesheet.compile(unquote(platform), rules))
+          end
+        end)
+      end
+    end
+  end
+
+end
---
+++ test/live_view_native_stylesheet_test.exs
@@ -0,0 +1,68 @@
+defmodule LiveViewNativeStylesheetTest do
+  use ExUnit.Case
+  doctest LiveViewNative.Stylesheet
+
+  defmodule MockCompiler do
+    def compile(rules) do
+      compiled_rules = %{
+        "rule-1" => 1,
+        "rule-2" => 2,
+        "rule-3" => 3,
+        "rule-4" => 4,
+        "rule-5" => 5
+      }
+
+      String.split(rules, "\n", trim: true)
+      |> Enum.map(fn(rule) ->
+        Map.get(compiled_rules, rule)
+      end)
+    end
+  end
+
+  defmodule MockSheet do
+    use LiveViewNative.Stylesheet, :mock
+
+    def class("color-red", _target) do
+      """
+      rule-1
+      rule-3
+      """
+    end
+
+    def class("color-blue", target: :watch) do
+      """
+      rule-4
+      rule-5
+      """
+    end
+
+    def class("color-blue", _target) do
+      "rule-2"
+    end
+  end
+
+  setup do
+    Application.put_env(:live_view_native_stylesheet, :platforms, mock: MockCompiler)
+
+    on_exit(fn ->
+      Application.delete_env(:live_view_native_stylehseet, :platforms)
+    end)
+  end
+
+  test "will use registered platform compiler to compile ruleset" do
+    output = LiveViewNative.Stylesheet.compile(:mock, "rule-2")
+    assert output == [2]
+  end
+
+  test "will compile the rules for all listed classes" do
+    output = MockSheet.compile(["color-blue", "color-red"], target: nil)
+
+    assert output == %{"color-blue" => [2], "color-red" => [1,3]}
+  end
+
+  test "will compile the rules for a specific target" do
+    output = MockSheet.compile(["color-blue", "color-red"], target: :watch)
+
+    assert output == %{"color-blue" => [4,5], "color-red" => [1,3]}
+  end
+end
