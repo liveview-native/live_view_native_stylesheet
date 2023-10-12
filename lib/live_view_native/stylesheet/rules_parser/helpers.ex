bd6b105f2facd8cfb728d8d4f1e17898da34cc56
Nduati Kuria <012nkuria@gmail.com>
2023-10-12 18:28:18 +0300

    Refactor `RulesParser.Helpers`

--- lib/live_view_native/stylesheet/rules_parser/helpers.ex
+++ lib/live_view_native/stylesheet/rules_parser/helpers.ex
@@ -12,17 +12,17 @@ defmodule LiveViewNative.Stylesheet.RulesParser.Helpers do
     "underscore"
   ]
 
-  def parse(opts \\ []) do
-    additional_functions = Keyword.get(opts, :additional, [])
-
-    choice(Enum.map(@helper_functions ++ additional_functions, &string(&1)))
+  defp create_combinator(function_names) do
+    choice(Enum.map(function_names, &string(&1)))
     |> enclosed("(", variable(), ")")
     |> post_traverse({:to_function_call_ast, []})
     |> post_traverse({:tag_as_elixir_code, []})
   end
 
   defmacro __using__(opts \\ []) do
-    module_name = __MODULE__
+    additional_functions = Keyword.get(opts, :additional, [])
+
+    combinator = create_combinator(@helper_functions ++ additional_functions)
 
     quote do
       import NimbleParsec
@@ -30,9 +30,7 @@ defmodule LiveViewNative.Stylesheet.RulesParser.Helpers do
       import LiveViewNative.Stylesheet.SheetParser.PostProcessors,
         only: [to_function_call_ast: 5, to_elixir_variable_ast: 5, tag_as_elixir_code: 5]
 
-      defparsec(:helper_function, unquote(module_name).parse(unquote(opts)),
-        export_combinator: true
-      )
+      defparsec(:helper_function, unquote(Macro.escape(combinator)), export_combinator: true)
     end
   end
 end
