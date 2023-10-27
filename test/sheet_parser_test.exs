e25d7dc8a4465df8402b747219daccf98ce1d1b2
Nduati Kuria <012nkuria@gmail.com>
2023-10-27 14:11:01 +0300

    Do not strip comments in rules

--- test/sheet_parser_test.exs
+++ test/sheet_parser_test.exs
@@ -25,11 +25,12 @@ defmodule LiveViewNative.Stylesheet.SheetParserTest do
              ]
     end
 
-    test "comments are ignored" do
+    test "comments are ignored, unless they are part of the rules" do
       sheet = """
       # this is a comment that isn't included in the output
       "color-red" do
         color(.red)
+        # this is a comment will not be stripped
       end
       # this is a comment that isn't included in the output
       """
@@ -40,7 +41,7 @@ defmodule LiveViewNative.Stylesheet.SheetParserTest do
                {[
                   "color-red",
                   {:_target, [file: @file_name, line: 2, module: @module], Elixir}
-                ], "color(.red)\n"}
+                ], "color(.red)\n# this is a comment will not be stripped\n"}
              ]
     end
 
