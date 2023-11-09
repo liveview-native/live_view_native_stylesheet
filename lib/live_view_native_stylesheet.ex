b808960b2e6f36885d4b087909263545975466bb
May Matyi <may@matyi.net>
2023-11-08 18:08:17 -0800

    Allow using stylesheet modules

--- lib/live_view_native_stylesheet.ex
+++ lib/live_view_native_stylesheet.ex
@@ -1,8 +1,23 @@
 defmodule LiveViewNative.Stylesheet do
   defmacro __using__(format) do
+    stylesheet_module = __CALLER__.module
+
     case LiveViewNative.Stylesheet.RulesParser.fetch(format) do
       {:ok, parser} ->
         quote do
+          # This nested `__using__` macro is required to allow LiveViews
+          # and Live Components to inherit stylesheets via `use`
+          defmacro __using__(opts \\ []) do
+            inheriting_module = __CALLER__.module
+
+            # Append this module to working list of inherited stylesheets
+            quote bind_quoted: [opts: opts, inheriting_module: inheriting_module, stylesheet_module: unquote(stylesheet_module)] do
+              @__stylesheet_modules__ Module.get_attribute(inheriting_module, :__stylesheet_modules__, []) ++ [stylesheet_module]
+              defoverridable __stylesheet_modules__: 0
+              def __stylesheet_modules__, do: @__stylesheet_modules__
+            end
+          end
+
           import LiveViewNative.Stylesheet.SheetParser, only: [sigil_SHEET: 2]
           import LiveViewNative.Stylesheet.RulesHelpers
 
