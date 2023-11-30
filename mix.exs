fde796c1815c7ab481ede21ad1cd93426326c239
Brian Cardarella <bcardarella@gmail.com>
2023-11-30 14:34:32 -0500

    Updated README and added exdoc for release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule LiveViewNative.Stylesheet.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @scm_url "https://github.com/liveview-native/live_view_native_stylesheet"
+
   def project do
     [
       app: :live_view_native_stylesheet,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -19,13 +25,34 @@ defmodule LiveViewNative.Stylesheet.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Brian Cardarella", "Nduati Kuria"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @scm_url},
+      files:
+        ~w(lib CHANGELOG.md LICENSE.md mix.exs README.md .formatter.exs)
+    ]
+  end
+
+  defp description, do: "Stylesheet primitives for LiveView Native clients"
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      extras: ["README.md"]
+    ]
+  end
+
   defp elixirc_paths(:test), do: ["lib", "test/mocks"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_parsec, "~> 1.3"}
+      {:nimble_parsec, "~> 1.3"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
