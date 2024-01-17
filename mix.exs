95cb26f02afeefed8ebea5eafdb109daa15b8415
Brian Cardarella <bcardarella@gmail.com>
2024-01-17 12:30:24 -0500

    Preparnig 0.3.0-alpha.2 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LiveViewNative.Stylesheet.MixProject do
   use Mix.Project
 
-  @version "0.3.0-alpha.1"
+  @version "0.3.0-alpha.2"
   @scm_url "https://github.com/liveview-native/live_view_native_stylesheet"
 
   def project do
