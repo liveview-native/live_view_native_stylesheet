620df24df23c989e0055f66ab2c063d044313a20
Brian Cardarella <bcardarella@gmail.com>
2024-01-21 11:21:57 -0500

    Remove helpers (#43)
    * Remove helpers
    
    * Removed helpers module
    
    * Removed RulesHelpers

--- test/support/mock_rules_helpers.ex
+++
@@ -1,8 +0,0 @@
-defmodule MockRulesHelpers do
-  # Implements a parse/1 function that can be used to parse helper functions
-  use LiveViewNative.Stylesheet.RulesParser.Helpers, additional: ["to_abc"]
-
-  def to_abc(expr) when is_binary(expr) do
-    # ...
-  end
-end
