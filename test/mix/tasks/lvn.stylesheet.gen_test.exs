dd94a9980bb2b08430cab9800135a8702f9e1002
Brian Cardarella <bcardarella@gmail.com>
2024-05-01 20:20:34 -0400

    test umbrella

--- test/mix/tasks/lvn.stylesheet.gen_test.exs
+++ test/mix/tasks/lvn.stylesheet.gen_test.exs
@@ -10,19 +10,44 @@ defmodule Mix.Tasks.Lvn.Stylesheet.GenTest do
     :ok
   end
 
-  test "generates the stylesheet for the request format into the `styles` directory", config do
-    in_tmp_live_project config.test, fn ->
-      Stylesheet.Gen.run(["mock", "App"])
-      assert_file "lib/live_view_native_stylesheet_web/styles/app.mock.ex"
+
+  describe "when a single app" do
+    test "generates the stylesheet for the request format into the `styles` directory", config do
+      in_tmp_live_project config.test, fn ->
+        Stylesheet.Gen.run(["mock", "App"])
+        assert_file "lib/live_view_native_stylesheet_web/styles/app.mock.ex"
+      end
+    end
+
+    test "will raise with message if invalid format is given", config do
+      in_tmp_live_project config.test, fn ->
+        assert_raise(Mix.Error, fn() ->
+          Stylesheet.Gen.run(["other"])
+        end)
+        refute_file "lib/live_view_native_stylesheet_web/styles/app.mock.ex"
+      end
     end
   end
 
-  test "will raise with message if invalid format is given", config do
-    in_tmp_live_project config.test, fn ->
-      assert_raise(Mix.Error, fn() ->
-        Stylesheet.Gen.run(["other"])
-      end)
-      refute_file "lib/live_view_native_stylesheet_web/styles/app.mock.ex"
+  describe "when an umbrella app" do
+    test "generates the stylesheet for the request format into the `styles` directory", config do
+      in_tmp_live_umbrella_project config.test, fn ->
+        File.cd!("live_view_native_stylesheet_web", fn ->
+          Stylesheet.Gen.run(["mock", "App"])
+          assert_file "lib/live_view_native_stylesheet_web/styles/app.mock.ex"
+        end)
+      end
+    end
+
+    test "will raise with message if invalid format is given", config do
+      in_tmp_live_umbrella_project config.test, fn ->
+        File.cd!("live_view_native_stylesheet_web", fn ->
+          assert_raise(Mix.Error, fn() ->
+            Stylesheet.Gen.run(["other"])
+          end)
+          refute_file "lib/live_view_native_stylesheet_web/styles/app.mock.ex"
+        end)
+      end
     end
   end
 end
