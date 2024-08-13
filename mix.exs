a7d9b3574dd758ff19987eee3d625dad1847e7cc
Brian Cardarella <bcardarella@gmail.com>
2024-08-13 11:42:18 -0400

    0.3.0-rc.5

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LiveViewNative.Stylesheet.MixProject do
   use Mix.Project
 
-  @version "0.3.0-rc.4"
+  @version "0.3.0-rc.5"
   @source_url "https://github.com/liveview-native/live_view_native_stylesheet"
 
   def project do
