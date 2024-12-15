3baed50f81e401ca93193e10ef60eeb44d1d673c
Brian Cardarella <bcardarella@gmail.com>
2024-12-15 15:43:48 -0500

    Use git main for lvn

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule LiveViewNative.Stylesheet.MixProject do
 
   defp deps do
     [
-      {:live_view_native, "~> 0.3.1"},
+      {:live_view_native, github: "liveview-native/live_view_native"},
+      # {:live_view_native, "~> 0.3.1"},
       {:nimble_parsec, "~> 1.3"},
       {:floki, ">= 0.30.0", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
